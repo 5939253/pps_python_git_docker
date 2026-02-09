74d672859c25f361cd6f24f17140f34148346b67
miguelonrs45 <miguelonrs45@gmail.com>
2026-02-09 16:20:54 +0100

    Experto Python: app.py actualizado para pruebas y requirements listos

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("Hola Mundo!!")
+from bayeta import frotar
+
+# Probamos la función pidiendo, por ejemplo, 3 frases
+frases_de_prueba = frotar(3)
+
+print("--- Probando la Bayeta de la Fortuna ---")
+for i, frase in enumerate(frases_de_prueba, 1):
+    print(f"{i}. {frase}")
